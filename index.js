8d3870187917c964b5c2159519ec5f9e0e044328
Jayalls <jason.allais@outlook.com>
2022-01-26 07:09:57 +0100

    Dernière version de index.js

--- index.js
+++ index.js
@@ -11,7 +11,36 @@ const fetchUser = async () => {
 const userDisplay = async () => {
   await fetchUser();
 
-  document.body.innerHtml = userData.map;
+  const dateParser = (date) => {
+    let newDate = new Date(date).toLocaleDateString("fr-FR", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    return newDate;
+  };
+
+  const dayCalc = (date) => {
+    let today = new Date();
+    let todayTimestamp = Date.parse(today);
+    let timestamp = Date.parse(date);
+
+    return Math.ceil((todayTimestamp - timestamp) / 8.64e7);
+  };
+
+  document.body.innerHTML = userData
+    .map(
+      (user) =>
+        `
+        <div class="card">
+          <img src="${user.picture.large}">
+          <h3>${user.name.first} ${user.name.last}</h3>
+          <p>${user.location.city}, ${dateParser(user.dob.date)}</p>
+          <em>Membre depuis: ${dayCalc(user.registered.date)} jours</em>
+        </div>
+  `
+    )
+    .join("");
 };
 
 userDisplay();
